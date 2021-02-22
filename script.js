ccc19a5f846112369b3f8e7456a65f4109509738
Sara Carlstein <carlstein.sara@gmail.com>
2021-02-22 16:58:05 +0100

    added features

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const API_KEY = "f8a490f34874ecd05c790250dd59f6e9",
   APP_ID = "9c4182eb",
   API_URL = "https://api.edamam.com/search?";
 
-let searchQuery = "cucumber";
+/* let searchQuery = "fish";
 const searchResults = document.getElementById("searchResults");
 
+
+
+console.log(searchField) */
+
+
+
 const searchRecipes = () => {
+  let searchQuery = document.getElementById("searchBar").value
   fetch(`${API_URL}q=${searchQuery}&app_id=${APP_ID}&app_key=${API_KEY}&to=10`)
     .then((response) => response.json())
     .then((data) => {
